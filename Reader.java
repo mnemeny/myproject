03c232a0f1c8b637ad68d3a306c63e2020cae0c2
mnemeny <mnemeny@yandex.ru>
2014-11-13 13:41:59 +0400

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,17 +1,8 @@
 public class eHandler implements ActionListener{
-		public void actionPerformed(ActionEvent e) {
-		
-//добавляем операцию проверки введенных данных
-		try{
-
-//создаем условие при котором будет выполнятся данный слушатель
-			if(e.getSource()==button1){
-
-
-//вывод в ответ результата и его округление
-				a = "Ответ: " +(double)Math.round(y*100)/100 +" кв.м.  Осталость места: "+ x+" кв.м.";
-				label.setText(a);
-			}
-//выводим сообщение о вводе неправильных данных
-		}catch (Exception ex){JOptionPane.showMessageDialog(null,"Вы что-то ввели не так");}
-		}
+public void actionPerformed(ActionEvent e) {
+try{
+if(e.getSource()==button1){
+x1 = Integer.parseInt(text1.getText());
+x2 = Integer.parseInt(text2.getText());
+x3 = Integer.parseInt(text3.getText());
+x = Integer.parseInt(text.getText());
